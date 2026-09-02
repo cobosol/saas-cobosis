e72645aa966b389a6297ac9afc892c297391f188
Cobosol <ycocab@gmail.com>
2026-09-01 21:40:35 -0400

    Mejoras de navegabilidad

--- servicios/urls.py
+++ servicios/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('servicios/<slug:slug>/', views.detalle_servicio, name='detalle_servicio'),
-    path('servicios/', views.servicios, name='servicios'),
+    #path('servicios/', views.servicios, name='servicios'),
     path('suscribirse/<int:plan_id>/', views.procesar_suscripcion, name='procesar_suscripcion'),
     # Agregar rutas similares para otros productos...
 ]
